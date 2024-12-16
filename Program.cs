6017c5141fd842d4f369a31697b3099fd01fd1eb
bass <msileha@mail.ru>
2024-12-16 18:14:46 +0300

    qwe

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace lesson16_12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        func_1(2, 10, false);
+    }
+
+    public static void func_1(int a, int b, bool c){
+        for (int i = a; i <= b; i++){
+            if(c == true){
+                if(i % 2 == 0){
+                    Console.WriteLine(i);
+                }
+            }
+            else{
+                if(i % 2 != 0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
